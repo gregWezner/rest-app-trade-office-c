2022eb4d98f5bb274ad0d0e247b4618aeff59b1d
gregWezner <weznerowicz@gmail.com>
2016-04-19 16:22:48 +0200

    company api changed

--- src/main/java/hello/CompanyController.java
+++ src/main/java/hello/CompanyController.java
@@ -7,18 +7,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class GreetingController {
+public class CompanyController {
 
     private static Map<String, Company> companyByName = new HashMap<>();
+    private static Map<String, Company> companyBySymbol = new HashMap<>();
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public HttpStatus save(@RequestBody Company company) {
         companyByName.put(company.getName(), company);
+        companyBySymbol.put(company.getSymbol(), company);
         return HttpStatus.OK;
     }
 
-    @RequestMapping("/read/company")
+    @RequestMapping("/read/name")
     public Company readByCompany(@RequestParam String name) {
+        return companyByName.get(name);
+    }
+
+    @RequestMapping("/read/symbol")
+    public Company readBySymbol(@RequestParam String name) {
         return companyByName.get(name);
     }
 
