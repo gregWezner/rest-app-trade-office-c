50b211f6e64f803f3bd4d761378f7464c17115d4
gregWezner <weznerowicz@gmail.com>
2016-04-19 16:16:55 +0200

    company api changed

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,35 +1,25 @@
 package hello;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.*;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private static Map<String, Company> companyByName = new HashMap<>();
 
     @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public HttpStatus save(@RequestBody Company company) throws IOException {
-        FileOutputStream fout = new FileOutputStream("output.ser");
-        try (ObjectOutputStream oos = new ObjectOutputStream(fout)) {
-            oos.writeObject(company);
-        }
+    public HttpStatus save(@RequestBody Company company) {
+        companyByName.put(company.getName(), company);
         return HttpStatus.OK;
     }
 
-    @RequestMapping("/read")
-    public Company read() throws IOException, ClassNotFoundException {
-        FileInputStream fin = new FileInputStream("output.ser");
-        try (ObjectInputStream reader = new ObjectInputStream(fin)) {
-            return (Company) reader.readObject();
-        }
+    @RequestMapping("/read/company")
+    public Company readByCompany(@RequestParam String name) {
+        return companyByName.get(name);
     }
 
 }
